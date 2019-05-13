66a4ac62b0a6d5f9ed5e7209f8d49be5662a055a
13262218151@163.com <13262218151@163.com>
2019-05-13 23:54:23 +0800

    modify 2.6-4

--- PythonCode/code_test/2.6-4_test.py
+++ PythonCode/code_test/2.6-4_test.py
@@ -2,7 +2,7 @@ dec=ord('a')
 bina=bin(dec)
 octo=oct(dec)
 hexa=hex(dec)
-print('%d,%.2f,%d,%d,'%(dec,dec/3,int(dec/3),dec%3))
+print('%d,%.2f,%d,%d,'%(dec,dec/3,dec//3,dec%3))
 print('bin='+bina,'oct='+octo,'hex='+hexa)
 
 
