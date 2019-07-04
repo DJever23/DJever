9c95510df6c5afeefac3ea4f8f6c37b95b36c689
dengjie <dengjie@foeec.com>
2019-07-04 20:44:45 +0800

    9.23&test_1

---
+++ C_code/test_9.10.1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void shownumber(int m,int n);
+
+int main()
+{
+    int a,b;
+    printf("请输入两个数字：\n");
+    printf("num1=");
+    scanf("%d",&a);
+    printf("num2=");
+    scanf("%d",&b);
+    
+    shownumber(a,b);
+    return 0;
+}
+
+void shownumber(int m,int n)
+{
+   if(m>n)
+      printf("较大的数是%d\n",m);
+    else
+      printf("较大的数是%d\n",n);
+}
